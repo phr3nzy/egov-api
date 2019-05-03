a3367cd8ae8799b0f02dbf331a35671dc9f0c022
phr3nzy <adilosama47@gmail.com>
2019-05-03 08:28:35 +0200

    Add: user api checks

---
+++ lib/middleware/checks/users.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { HTTP400Error } from "../../utils/httpErrors";
+
+export const newUserParams = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	if (!req.body) {
+		throw new HTTP400Error("Something went wrong. Please try again");
+	} else {
+		if (!req.body.username) {
+			throw new HTTP400Error("Username is required");
+		}
+		if (!req.body.email) {
+			throw new HTTP400Error("Emal is required");
+		}
+		if (!req.body.password) {
+			throw new HTTP400Error("Password is required");
+		}
+		next();
+	}
+};
