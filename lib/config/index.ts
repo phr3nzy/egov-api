e1fd878eda5807524187d51f9acaa65677b07bd1
phr3nzy <adilosama47@gmail.com>
2019-05-03 08:23:28 +0200

    Add: mongoose database initiator and user schema

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -1,8 +1,9 @@
-require("dotenv").config()
-
+require("dotenv").config();
 
 export const env = process.env.NODE_ENV;
 
 export const secret = process.env.SECRET;
 
 export const api_version = process.env.API_VER;
+
+export const DB_URL = process.env.DB_URL;
---
+++ lib/db/users/index.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+	username: {
+		type: String,
+		required: [true, "Username is required"],
+		unique: true,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: [true, "Email is required"],
+		unique: true,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: [true, "Password is required"],
+		unique: true,
+		trim: true
+	},
+	firstName: {
+		type: String,
+		required: [true, "First name is required"]
+	},
+	lastName: {
+		type: String,
+		required: [true, "Last name is required"]
+	}
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
